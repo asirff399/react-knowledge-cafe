a973595179ea281d7d52dcb03527d19c005b47e1
Asir Intisar <aintisar48@gmail.com>
2025-01-12 22:21:38 +0600

    [fetch] - blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarkes from './components/Bookmarkes/Bookmarkes'
 import Header from './components/header/Header'
 
 function App() {
@@ -7,7 +8,10 @@ function App() {
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className='w-4/6 mx-auto flex'>
+        <Blogs></Blogs>
+        <Bookmarkes></Bookmarkes>
+      </div>
     </>
   )
 }
---
+++ src/components/Bookmarkes/Bookmarkes.jsx
@@ -0,0 +1,9 @@
+const Bookmarkes = () => {
+    return (
+        <div className="w-1/3">
+            <h2>Bookmarkes</h2>
+        </div>
+    );
+};
+
+export default Bookmarkes;
